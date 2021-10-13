76270b6f158ffdf94be136ea26392f25b6ef6d70
Daniil <dan2002s@yandex.ru>
2021-10-13 14:36:23 +0300

    Lab1

---
+++ ConsoleApp1/B.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class B : A
+    {
+        public object a1;
+        public object a2;
+        public object a3;
+        public object a4;
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a1 = new A();
+            A a2 = new A();
+            A a3 = new A();
+            A a4 = new A();
+            B b5 = new B();
+            b5.a1 = a1; //и тд
+            b5.a2 = a2;
+            b5.a3 = a3;
+            b5.a4 = a4;
+        }
+    }
+}
